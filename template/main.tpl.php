2f02faa07bbf717e8e2e7b6f1b99fbc37580fee2
taylorchu <tailinchu@gmail.com>
2011-07-19 16:33:56 -0700

    Edited template/main.tpl.php via GitHub

--- template/main.tpl.php
+++ template/main.tpl.php
@@ -13,10 +13,10 @@ header('Content-Type: text/html; charset=UTF-8');
 <meta charset = "UTF-8"/>
 <meta name = "description" content = "<?php echo $out['subtitle'];?>"/>
 <title><?php echo $out['subtitle'];?> - <?php echo $config['title'];?></title>
-<script src = "http://code.jquery.com/jquery.min.js"></script>
 <link rel = "stylesheet" type = "text/css" href = "theme/<?php echo $config['theme'];?>.thm.css"/>
 <link rel = "alternate" type = "application/atom+xml" href = "feed.php?topic" title = "<?php echo $lang['topic'];?> - <?php echo $config['title'];?>"/>
 <link rel = "alternate" type = "application/atom+xml" href = "feed.php?reply" title = "<?php echo $lang['reply'];?> - <?php echo $config['title'];?>"/>
+<script src = "http://code.jquery.com/jquery.min.js"></script>
 <?php echo hook('head');?>
 </head>
 <body>
