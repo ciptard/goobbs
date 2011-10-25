7d3711dd2d33a896400cb1b3f5218f98a532c0ce
root <root@localhost.localdomain>
2011-10-24 22:06:28 -0700

    clean up candy.js

--- js/candy.js
+++ js/candy.js
@@ -1,13 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	$("a, img, input[type='submit'], select").hover(
 		function() {
-		           $(this).animate({
-		                opacity: 0.5
-		            }, "fast");
+			$(this).animate({opacity: 0.5}, "fast");
 		},
 		function() {
-		            $(this).animate({
-		            	opacity: 1
-		            }, "fast");
-	});
+			$(this).animate({opacity: 1}, "fast");
+		}
+	);
 });
