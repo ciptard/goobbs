d2e378da34123e6b341091a67fbedb0fa3e3021c
Your Name <march@localhost.localdomain>
2011-10-26 09:32:48 -0700

    restrict bbcode pattern

--- include/parser.inc.php
+++ include/parser.inc.php
@@ -5,31 +5,31 @@ function bbcode($text)
 	//the pattern to be matched
 	//the replacement
 
-	$pattern[] = '%\[b\]([^\[\]]+)\[/b\]%';
+	$pattern[] = '%\[b\]([^[]+)\[/b\]%';
 	$replace[] = '<b>$1</b>';
 
-	$pattern[] = '%\[i\]([^\[\]]+)\[/i\]%';
+	$pattern[] = '%\[i\]([^[]+)\[/i\]%';
 	$replace[] = '<i>$1</i>';
 
-	$pattern[] = '%\[u\]([^\[\]]+)\[/u\]%';
+	$pattern[] = '%\[u\]([^[]+)\[/u\]%';
 	$replace[] = '<ins>$1</ins>';
 
-	$pattern[] = '%\[s\]([^\[\]]+)\[/s\]%';
+	$pattern[] = '%\[s\]([^[]+)\[/s\]%';
 	$replace[] = '<del>$1</del>';
 
-	$pattern[] = '%\[img\]([^\[\]]+)\[/img\]%';
+	$pattern[] = '%\[img\]([^[]+)\[/img\]%';
 	$replace[] = '<img src="$1" alt=""/>';
 
-	$pattern[] = '%\[url=([^\[\]]+)\]([^\[\]]+)\[/url\]%';
+	$pattern[] = '%\[url=([^[]+)\]([^[]+)\[/url\]%';
 	$replace[] = '<a href="$1">$2</a>';
 
 	$pattern[] = '%\[youtube\]([-\w]{11})\[/youtube\]%';
 	$replace[] = '<iframe width="320" height="240" src="http://www.youtube.com/embed/$1?rel=0" frameborder="0"></iframe>';
 
-	$pattern[] = '%\[block\]([^\[\]]+)\[/block\]%';
+	$pattern[] = '%\[block\]([^[]+)\[/block\]%';
 	$replace[] = '<div class="block">$1</div>';
 	
-	$pattern[] = '%\[quote\]([^\[\]]+)\[/quote\]%';
+	$pattern[] = '%\[quote\](\d{4}-\d{2}-\d{8}[a-z\d]{5})\[/quote\]%';
 	$replace[] = '<a href="#$1">&gt; $1</a>';
 	//the variable for the replace
 	return preg_replace($pattern, $replace, $text);
