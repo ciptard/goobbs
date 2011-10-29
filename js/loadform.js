e6b987004645af48faab6135e97368492b44d237
root <root@localhost.localdomain>
2011-10-29 08:32:41 -0700

    add quick reply

---
+++ js/loadform.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('a.important[href^="add.php"]').click(function(event) {
+		event.preventDefault();
+		$('#form').remove();
+		$('<div id="form"></div>').load($(this).attr('href') + ' form').insertAfter(this);
+	});
+});
--- plugin/common.plg.php
+++ plugin/common.plg.php
@@ -4,7 +4,8 @@ function common_head()
 {
 	return '<script src="js/candy.js"></script>
 	<script src="js/bbcode.js"></script>
-	<script src="js/highlight.js"></script>';
+	<script src="js/highlight.js"></script>
+	<script src="js/loadform.js"></script>';
 }
 
 ?>
