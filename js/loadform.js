450a144514bb70073db676e620fcb330588607c3
taylorchu <tailinchu@gmail.com>
2011-11-07 14:47:53 -0800

    Update js/loadform.js

--- js/loadform.js
+++ js/loadform.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
 	$('a.important[href^="add.php"]').click(function(event) {
 		event.preventDefault();
-		$('#form').remove();
-		$('<div id="form"></div>').hide().insertAfter(this).load($(this).attr('href') + ' form', function() {
-			bbcode();
-			$(this).slideDown('slow');
-		});
+		if($(this).siblings('#form').length > 0) {
+			$(this).siblings('#form').slideUp('slow', function(){ $(this).remove(); });
+		} else {
+			$('#form').remove();
+			$('<div id="form"></div>').hide().insertAfter(this).load($(this).attr('href') + ' form', function() {
+				bbcode();
+				$(this).slideDown('slow');
+			});
+		}
 	});
 });
