a7503cdd2dd6381ae8eaff955203263fdbd1a195
taylorchu <tailinchu@gmail.com>
2011-11-11 02:38:00 -0800

    Update include/page.inc.php

--- include/page.inc.php
+++ include/page.inc.php
@@ -3,9 +3,9 @@
 function pageControl($p, $total, $loc)
 {
 	return '<div id="page"><ul>' .
-	($p >= 2? '<li><a href="?' .$loc. '&amp;p=' .($p-1). '">← ' .($p-1). '</a></li>' : '').
+	($p > 1? '<li><a href="?' .$loc. '&amp;p=' .($p-1). '">← ' .($p-1). '</a></li>' : '').
 	'<li>' .$p. ' / ' .$total. '</li>' .
-	($p <= $total-1? '<li><a href="?' .$loc. '&amp;p=' .($p+1). '">' .($p+1). ' →</a></li>' : '').
+	($p < $total? '<li><a href="?' .$loc. '&amp;p=' .($p+1). '">' .($p+1). ' →</a></li>' : '').
 	'</ul></div>';
 }
 
