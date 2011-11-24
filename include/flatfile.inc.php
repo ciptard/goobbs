b42d3e2840a04b5fcbdc0c4301077153b4d632c8
taylorchu <tailinchu@gmail.com>
2011-11-24 14:04:45 -0800

    Update include/flatfile.inc.php

--- include/flatfile.inc.php
+++ include/flatfile.inc.php
@@ -26,9 +26,28 @@ function readEntry($type, $file)
 	return json_decode(substr(file_get_contents('data/' .$type. '/' .$file. '.dat.php'), 13), true);
 }
 
+function _json_encode($var)
+{
+	$list = array();
+	foreach($var as $key => $value)
+	{
+		list($key, $value) = str_replace(
+			array('"', "\r", "\n", "\t"), 
+			array('\"', '\r', '\n', '\t'), 
+			array($key, $value));
+		$tmp = '"' .$key. '": ';
+		if(is_array($value))
+			$tmp .= _json_encode($value);
+		else
+			$tmp .= '"' .$value. '"';
+		$list[] = $tmp;
+	}
+	return '{' .implode(',', $list). '}';
+}
+
 function saveEntry($type, $file, $data)
 {
-	file_put_contents('data/' .$type. '/' .$file. '.dat.php', '<?php exit;?>' . "\n" .json_encode($data));
+	file_put_contents('data/' .$type. '/' .$file. '.dat.php', '<?php exit;?>' . "\n" ._json_encode($data));
 }
 
 function deleteEntry($type, $file)
