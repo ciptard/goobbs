1ca08be58565cf2582327f5e23fb515ba260c739
Your Name <march@localhost.localdomain>
2011-12-18 19:59:17 -0800

    improve image zoomer

--- plugin/core/js/imgzoom.js
+++ plugin/core/js/imgzoom.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
 	$('.img').hover(
 		function() {
-			$('img').attr('src', $(this).attr('src'))
-			.css({
-				'position': 'absolute',
-				'top': '50%',
-				'left': '50%'
-			}).appendTo('body').hide().fadeIn().
-			.click(function() {
-				$(this).remove();
-			});
+			if($('#zoom').length == 0) {
+				$('<img id="zoom">').attr('src', $(this).attr('src'))
+				.css({
+					'position': 'absolute',
+					'max-width': '800px',
+					'max-height': '600px',
+					'box-shadow': '0 0 100px #ccc'
+				}).appendTo('body').hide().fadeIn()
+				.click(function() {
+					$('#zoom').remove();
+				});
+				$('#zoom').css({
+					'top': ($(window).height() - $('#zoom').height())/2 + 'px',
+					'left': ($(window).width() - $('#zoom').width())/2 + 'px'
+				});
+			}
 		},
 		function() {}
 	);
