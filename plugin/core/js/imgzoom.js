652ab9f1fe2992da1d1d75e281b42c46bf4f52d9
taylorchu <tailinchu@gmail.com>
2011-12-18 22:46:20 -0800

    Update plugin/core/js/imgzoom.js

--- plugin/core/js/imgzoom.js
+++ plugin/core/js/imgzoom.js
@@ -1,23 +1,18 @@
 $(document).ready(function(){
-	$('.img').hover(
-		function() {
-			if($('#zoom').length == 0) {
-				$('<img id="zoom">').attr('src', $(this).attr('src'))
-				.css({
-					'position': 'absolute',
-					'max-width': '800px',
-					'max-height': '600px',
-					'box-shadow': '0 0 100px #ccc'
-				}).appendTo('body').hide().fadeIn()
-				.click(function() {
-					$('#zoom').remove();
-				});
-				$('#zoom').css({
-					'top': ($(window).height() - $('#zoom').height())/2 + 'px',
-					'left': ($(window).width() - $('#zoom').width())/2 + 'px'
-				});
-			}
-		},
-		function() {}
-	);
+	$('.img').click(function() {
+		if($('#zoom').length == 0) {
+			var offset = $(this).offset();
+			$('<img id="zoom"/>').attr('src', $(this).attr('src'))
+			.css({
+				'position': 'absolute',
+				'max-width': '800px',
+				'top': offset.top,
+				'left': offset.left,
+				'box-shadow': '0 0 100px #ccc'
+			}).appendTo('body').hide().fadeIn()
+			.click(function() {
+				$('#zoom').remove();
+			});
+		}
+	});
 });
