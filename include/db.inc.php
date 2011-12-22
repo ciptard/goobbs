c77af05bf0fda8b2f9a1c34ab5d76f2750b3b7c2
taylorchu <tailinchu@gmail.com>
2011-12-21 16:30:58 -0800

    Update include/db.inc.php

--- include/db.inc.php
+++ include/db.inc.php
@@ -1,64 +1,13 @@
 <?php
 
-function _json_decode($json)
-{
-	$quote = false;
-	$php = '';
-	$end = strlen($json);
-	for($i=0; $i<$end; $i++)
-	{
-		if($quote)
-		{
-			if ($json[$i] === '$')
-				$php .= '\$';
-			else
-				$php .= $json[$i];
-		}
-		else
-		{
-			if ($json[$i] === '{')
-				$php .= 'array(';
-			else if ($json[$i] === '}')
-				$php .= ')';
-			else if ($json[$i] === ':')
-				$php .= '=>';
-			else
-				$php .= $json[$i];
-		}
-		if ($json[$i] === '"' && $json[$i-1] !== '\\')
-			$quote = !$quote;
-	}
-	return eval('return ' .$php. ';');
-}
-
-function _json_encode($var)
-{
-	$list = array();
-	foreach($var as $key => $value)
-	{
-		$tmp = '"' .jsonEsc($key). '":';
-		if(is_array($value))
-			$tmp .= _json_encode($value);
-		else
-			$tmp .= '"' .jsonEsc($value). '"';
-		$list[] = $tmp;
-	}
-	return '{' .implode(',', $list). '}';
-}
-
-function jsonEsc($str)
-{
-	return str_replace(array('"', "\r", "\n", "\t"), array('\"', '\r', '\n', '\t'), $str);
-}
-
 function readEntry($type, $file)
 {
-	return _json_decode(substr(file_get_contents('data/' .$type. '/' .$file. '.dat.php'), 14));
+	return eval('return ' .substr(file_get_contents('data/' .$type. '/' .$file. '.dat.php'), 14));
 }
 
 function saveEntry($type, $file, $data)
 {
-	file_put_contents('data/' .$type. '/' .$file. '.dat.php', "<?php exit;?>\n" ._json_encode($data));
+	file_put_contents('data/' .$type. '/' .$file. '.dat.php', "<?php exit;?>\n" .var_export($data, true));
 }
 
 function deleteEntry($type, $file)
