ec3ce391bd15b80f10b67c795a7e1d92386a865c
taylorchu <tailinchu@gmail.com>
2012-02-01 11:11:30 -0800

    Update plugin/core/js/loadform.js

--- plugin/core/js/loadform.js
+++ plugin/core/js/loadform.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
 		} else {
 			$('#form').remove();
 			$('<div id="form"></div>').hide().insertAfter(this).load($(this).attr('href') + ' form', function() {
-				bbcode();
 				$(this).slideDown('slow');
 			});
 		}
