78979e275babe83c9f708a4cb5fae396acf46e56
taylorchu <march@localhost.localdomain>
2012-02-22 20:44:45 +0000

    fix row issue

--- view.php
+++ view.php
@@ -23,7 +23,7 @@ if(isGET('topic') && isValidEntry('topic', $_GET['topic']))
 		<li>' .$out['subtitle']. ' <span class="divider">/</span></li>
 		<li>' .$lang['count']. ' (' .(count($topicEntry['reply']) + 1). ')</li>
 	</ul>
-	<div class="well row">
+	<div class="well container">
 	<div class="span3"><p class="user">' .manageTopic($_GET['topic']).$topicEntry['trip']. '</p>
 	<p>' .toDate($_GET['topic']). '</p></div>
 	<div class="span7"><div class="content">' .content($topicEntry['content']). '</div>'.
@@ -37,7 +37,7 @@ if(isGET('topic') && isValidEntry('topic', $_GET['topic']))
 		foreach(viewPage($topicEntry['reply'], $p) as $reply)
 		{
 			$replyEntry = readEntry('reply', $reply);
-			$out['content'] .= '<div id="' .$reply. '" class="well row">
+			$out['content'] .= '<div id="' .$reply. '" class="well container">
 			<div class="span3"><p class="user">' .manageReply($reply).$replyEntry['trip']. '</p>
 			<p>' .toDate($reply). '</p></div>
 			<div class="span7"><div class="content">' .content($replyEntry['content']). '</div>'.
