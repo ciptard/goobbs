ddcc2565964613d6eda831089f2fa9e774fb2bd9
taylorchu <march@localhost.localdomain>
2012-02-23 12:53:44 +0000

    rewrite load reply

--- plugin/core/js/loadreply.js
+++ plugin/core/js/loadreply.js
@@ -1,27 +1,11 @@
 $(function(){
-	$('a.label-info[href^="view.php"]').hover(
-		function(e) {
+	$('a.label-info[href^="view.php"]').popover({
+		'title': 'quote',
+		'content': function() {
 			var link = $(this).attr('href').split('#');
 			var target = link[0] + ' #' + link[1] + ' .content';
-			$('<div id="reply" class="well"></div>').hide().appendTo('body').load(target, function() {
-				$(this).css({
-					'width': '600px',
-					'background-color': '#FFF',
-					'position': 'absolute',
-					'top': e.pageY+20,
-					'left': e.pageX+20
-				})
-				.fadeIn();
-			});
-		},
-		function() {
-			$('#reply').remove();
+			$.ajaxSetup({async:false});
+			return $('<div></div>').load(target).html();
 		}
-	).
-	mousemove(function(e) {
-		$('#reply').css({
-			'top': e.pageY+20,
-			'left': e.pageX+20
-		})
 	});
 });
