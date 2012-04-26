6d4c8da9ddabab823b6eb8ab468d4b6f0388cdef
taylorchu <march@localhost.localdomain>
2012-04-25 23:50:25 -0700

    update bbcode.js

--- plugin/core/js/bbcode.js
+++ plugin/core/js/bbcode.js
@@ -8,15 +8,15 @@ function bbcode() {
 	$.each(['b', 'i', 'u', 's', 'img', 'url', 'youtube', 'block'], function (i, tag) {
 		$('<input class="btn" type="button"/>').attr('value', tag).appendTo(bar);
 	});
-	$('<input id="upload-file" type="file" multiple="multiple"/>').insertBefore(ta);
+	$('<div><input id="upload-file" type="file" multiple="multiple"/></div>').insertBefore(ta);
 	$(bar).insertBefore(ta);
 
 	
 	$('#upload-file').change(function() {
-	  $.each($("#upload-file")[0].files, function(i, file) {
+	  $.each($('#upload-file')[0].files, function(i, file) {
 		var fd = new FormData();
-		fd.append("image", file);
-		fd.append("key", "6528448c258cff474ca9701c5bab6927");
+		fd.append('image', file);
+		fd.append('key', '6528448c258cff474ca9701c5bab6927');
 		
 		var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = function () {
@@ -24,12 +24,12 @@ function bbcode() {
 			ta.value += '[img]' + JSON.parse(xhr.responseText).upload.links.large_thumbnail + '[/img]';
 		  }
 		}
-		xhr.open("POST", "http://api.imgur.com/2/upload.json"); // Boooom!
+		xhr.open('POST', 'http://api.imgur.com/2/upload.json');
 		xhr.send(fd);
 	  });
 	});
 
-	$('#bbcode input').click(function() {
+	$('#bbcode .btn').click(function() {
 		var tag = $(this).attr('value');
 		var start = '['+tag+']';
 		var end = '[/'+tag+']';
