afea387a73e6713aafe1fea4fe16d50e101218ce
taylorchu <tailinchu@gmail.com>
2012-05-25 15:49:01 -0700

    fix load comment

--- plugin/core/js/loadreply.js
+++ plugin/core/js/loadreply.js
@@ -1,5 +1,5 @@
 $(function(){
-	$('a.label-info[href^="view.php"]').hover(function () {
+	$('a.label-info[href^="view.php"]').each(function () {
 		var target = this;
 		var link = $(target).attr('href').split('#');
 		$.getJSON('service.php/reply/' + link[1], function(json) {
